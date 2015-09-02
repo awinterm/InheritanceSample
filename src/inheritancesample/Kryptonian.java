8d7ab4803d3b9f988e73eac27ae24c9f9e9f9770
andre_000 <andre_000@DrewsLaptop.PK5001Z>
2015-09-02 10:12:28 -0500

    2nd commit

--- src/inheritancesample/Kryptonian.java
+++ src/inheritancesample/Kryptonian.java
@@ -10,5 +10,13 @@ package inheritancesample;
  * @author andre_000
  */
 public class Kryptonian extends Alien {
-    
+    private String sunPowered;
+
+    public String getSunPowered() {
+        return sunPowered;
+    }
+
+    public void setSunPowered(String sunPowered) {
+        this.sunPowered = sunPowered;
+    }
 }
